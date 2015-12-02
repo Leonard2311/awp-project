e5caf0bb0b612cecf80ebbda7cb54cb3ed3ef77f
Leonard2311 <leonardstanescu@gmail.com>
2015-12-03 00:34:13 +0200

    update forms.py

--- socialapp/forms.py
+++ socialapp/forms.py
@@ -2,6 +2,8 @@ from django.forms import Form, CharField, Textarea
 
 
 class UserPostForm(Form):
-    text = CharField(widget=Textarea(
-        attrs={'cols': 100, 'rows': 5}),
-        label="Enter your post here")
+    text = CharField(widget=Textarea(attrs={'cols': 100, 'rows': 5}), label="Enter your post here")
+
+
+class UserPostCommentForm(Form):
+    text = CharField(widget=Textarea(attrs={'cols': 100, 'rows': 5}), label="Enter your comment here")
